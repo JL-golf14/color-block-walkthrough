5b6309f78ec9d74297ba34e86adbfe74a5576340
Linh Kieu Le <linhkieule@Linhs-MacBook-Pro-2.local>
2017-02-15 10:28:13 -0600

    update

--- colorBlock.js
+++ colorBlock.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
 console.log("walkthrough");
 
-var color = ['aliceBlue', 'CornflowerBlue','Cyan', 'BlueViolet','PeachPuff','SlateBlue','PaleVioletRed'];
+var colorArray = ['aliceBlue', 'CornflowerBlue','Cyan', 'BlueViolet','PeachPuff','SlateBlue','PaleVioletRed'];
 //loops through the colors at i
-for (var i = 0; i < color.length; i++) {
+for (var i = 0; i < colorArray.length; i++) {
 
 
 //these four lines add a new block
 var newColorBlock = $('<div>');
-newColorBlock.css('background-color', color[i]);
+newColorBlock.css('background-color', colorArray[i]);
 newColorBlock.addClass('colorBlock');
 $('#colorBlockContainer').append(newColorBlock);
 
 
 }
-
-
+var randomNumberSelected = randomNumber(0,colorArray.length-1);
+var randomColor = colorArray[randomNumberSelected];
+$('#userColorPrompt').text(randomColor);
 });
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
